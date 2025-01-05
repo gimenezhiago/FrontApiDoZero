fac6c312de3f9a95b8d623a4f326a03c40ef672f
Hiago Gimenez <hiagogimenezvieira888@gmail.com>
2025-01-05 18:34:06 -0300

    uyghj

--- FrontApiDoZeroVite/src/pages/home/index.jsx
+++ FrontApiDoZeroVite/src/pages/home/index.jsx
@@ -1,13 +1,46 @@
-import './style.css'
+import "./style.css";
+import Trash from "../../assets/trash.svg";
 
 function Home() {
+  const users = [
+    {
+      id: "jfuhgfrfnie",
+      name: "Hiago",
+      age: 33,
+      email: "hiago@gmail.com",
+    },
+    {
+      id: "jfuhgfrfnieertr",
+      name: "Luana",
+      age: 345,
+      email: "linda@gmail.com",
+    },
+  ];
 
   return (
-      <div>
-          <h1>oiii</h1>
-      </div>
+    <div className="container">
+      <form action="">
+        <h1>Cadastro de Usuários</h1>
+        <input type="text" name="nome" placeholder="Nome" />
+        <input type="number" name="idade" placeholder="Idade" />
+        <input type="email" name="email" placeholder="Email" />
+        <button type="button">Cadastrar</button>
+      </form>
 
-  )
+      {users.map((user) => {
+        <div key={user.id} className="card">
+          <div>
+            <p>Nome: <span>{user.name}</span></p>
+            <p>Idade: <span>{user.age}</span></p>
+            <p>Email: <span>{user.email}</span></p>
+          </div>
+          <button>
+            <img src={Trash} />
+          </button>
+        </div>;
+      })}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
